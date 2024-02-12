5b1c347568ca72c45cf1b0659efd1bdfb85b3bd7
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-02-12 13:57:00 +0530

    JavaScript Practice Programs

---
+++ 20_javascriptProgram/04.js
@@ -0,0 +1,12 @@
+// Question-4 ---JavaScript Program to Convert Celsius to Fahrenheit
+
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+
+const celsiusTemperature = 25;
+const fahrenheitTemperature = celsiusToFahrenheit(celsiusTemperature);
+
+console.log(`${celsiusTemperature}°C is equal to ${fahrenheitTemperature}°F`);
