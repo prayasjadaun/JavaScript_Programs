b6655b8d730c08f3c1cb9e1b64ace25d080c36ef
Prayas Jadaun <prayashthakur6@gmail.com>
2024-02-16 13:16:08 +0530

    JavaScript Program Problem number 13 is Demonstrate the Use of Synchronous and Asynchronous Programming

---
+++ 20_javascriptProgram/13.js
@@ -0,0 +1,11 @@
+//Question-13--JavaScript Program to Demonstrate the Use of Synchronous and Asynchronous Programming
+
+// Synchronous
+console.log('Start Synchronous');
+console.log('End Synchronous');
+
+// Asynchronous
+console.log('Start Asynchronous');
+setTimeout(() => {
+    console.log('End Asynchronous');
+}, 1000);
