bbcba4ea63487d69cd6a1298b301ca643a676b1b
Prayas Jadaun <prayashthakur6@gmail.com>
2024-02-16 13:14:00 +0530

    JavaScript Program Problem number 12 is Demonstrate the Use of var, const, and let Keywords

---
+++ 20_javascriptProgram/12.js
@@ -0,0 +1,12 @@
+//Question-12--JavaScript Program to Demonstrate the Use of var, const, and let Keywords
+
+// var keyword (function-scoped)
+var variableVar = 10;
+
+// const keyword (block-scoped, immutable)
+const variableConst = 20;
+
+// let keyword (block-scoped, mutable)
+let variableLet = 30;
+
+console.log(`var: ${variableVar}, const: ${variableConst}, let: ${variableLet}`);
